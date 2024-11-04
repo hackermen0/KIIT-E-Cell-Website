50f794823d4a41fb8499fd4278ce81467352a7c9
Upaayan Naskar <upaayan@gmail.com>
2024-11-04 16:09:11 +0530

    Error handling in api endpoint

--- src/routes/api/hero/+server.ts
+++ src/routes/api/hero/+server.ts
@@ -1,15 +1,21 @@
 import type { RequestHandler } from './$types';
 import { json } from '@sveltejs/kit';
-import fs from "fs"
+import fs from 'fs/promises';
 
 export const GET: RequestHandler = async () => {
 
-    const content = fs.readFileSync("./static/Hero_data.json", "utf-8")
+    try {
 
-    const data = json(JSON.parse(content))
-
-    console.log(data)
-
-    return data
-
+        const content = await fs.readFile("./static/Hero_data.json", "utf-8")
+    
+        const data = JSON.parse(content)
+    
+        console.log(data)
+    
+        return json(data)
+    } catch(error){
+        console.error('Error reading file:', error);
+        return json({ error: 'File not found' }, { status: 500 });
+    }
 };
+
